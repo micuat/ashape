772a85459f074cbb00029e9a4cfc6a4936fa08d5
micuat <micuat@gmail.com>
2018-06-01 23:35:02 +0900

    024 stub

---
+++ 024_mesh/src/main.cpp
@@ -0,0 +1,13 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3, 2);    // <--- ofxShadertoy NEEDS the GL Programmable Renderer
+    settings.setSize(1024, 768);
+    ofCreateWindow(settings);       // <-------- setup the GL context
+    
+    // this kicks off the running of my app
+    ofRunApp(new ofApp());
+}
---
+++ 024_mesh/src/ofApp.cpp
@@ -0,0 +1,75 @@
+#include "ofApp.h"
+
+bool showfps = false;
+float fps;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    shadertoy.load("shaders/raymarch.frag");
+    ofSetFrameRate(60);
+    //camera.movespeed = 0.05;
+    //camera.rollspeed = camera.sensitivity = 0.2;
+    //camera.enableControls();
+    shadertoy.setAdvanceTime(true);
+    shadertoy.setCamera(&camera);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    shadertoy.draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+    if(showfps)ofDrawBitmapString(ofToString(ofGetFrameRate()), ofPoint(10, 10));
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    if (key == 'r') {
+        shadertoy.load("shaders/raymarch.frag");
+    }
+    if (key == ' ') {
+        showfps = !showfps;
+    }if(key == 'f') {
+		ofToggleFullscreen();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
