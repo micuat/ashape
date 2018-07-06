c8d958178e66b25fab1fb790f33d5516755ad3dd
micuat <micuat@gmail.com>
2018-07-07 01:04:13 +0900

    050 meshing

--- 050_openni/sketch.js
+++ 050_openni/sketch.js
@@ -1,4 +1,4 @@
-if(context == undefined) {
+if (context == undefined) {
   var context = null;
 }
 
@@ -8,9 +8,10 @@ var s = function (p) {
   let rotX = p.radians(180);  // by default rotate the hole scene 180deg around the x-axis, 
   // the data from openni comes upside down
   let rotY = p.radians(0);
+  let points;
 
   p.setup = function () {
-    p.createCanvas(1024, 768);
+    p.createCanvas(800, 800);
 
     if (context == null) {
       context = new Packages.SimpleOpenNI.SimpleOpenNI(p.that);
@@ -21,7 +22,7 @@ var s = function (p) {
       }
 
       // disable mirror
-      context.setMirror(false);
+      context.setMirror(true);
 
       // enable depthMap generation 
       context.enableDepth();
@@ -29,12 +30,17 @@ var s = function (p) {
 
     p.stroke(255, 255, 255);
     p.smooth();
-    p.perspective(p.radians(45),
-      parseFloat(p.width) / parseFloat(p.height),
-      10, 150000);
+
+    points = new Array(context.depthWidth() * context.depthHeight());
+    for (let i = 0; i < points.length; i++) {
+      points[i] = 0.0;
+    }
   }
 
   p.draw = function () {
+    p.perspective(p.radians(15),
+      parseFloat(p.width) / parseFloat(p.height),
+      10, 150000);
     // update the cam
     context.update();
 
@@ -46,34 +52,59 @@ var s = function (p) {
     p.scale(zoomF);
 
     let depthMap = context.depthMap();
-    let steps = 3;  // to speed up the drawing, draw every third point
+    let steps = 4;  // to speed up the drawing, draw every third point
     let index;
     let realWorldPoint;
 
     p.translate(0, 0, -1000);  // set the rotation center of the scene 1000 infront of the camera
 
     p.stroke(255);
+    p.strokeWeight(5);
 
     let realWorldMap = context.depthMapRealWorld();
 
-    // draw pointcloud
-    p.beginShape(p.LINES);
     for (let y = 0; y < context.depthHeight(); y += steps) {
       for (let x = 0; x < context.depthWidth(); x += steps) {
         index = x + y * context.depthWidth();
-        if (depthMap[index] > 0) {
-          // draw the projected point
-          //        realWorldPoint = context.depthMapRealWorld()[index];
-          realWorldPoint = realWorldMap[index];
-          p.vertex(realWorldPoint.x, realWorldPoint.y, realWorldPoint.z);  // make realworld z negative, in the 3d drawing coordsystem +z points in the direction of the eye
+        if (depthMap[index] > 0 && depthMap[index] < 1000) {
+          points[index] = p.lerp(points[index], realWorldMap[index].z, 0.05);
+        }
+      }
+    }
+    // draw pointcloud
+    p.noFill();
+    for (let y = 0; y < context.depthHeight() - steps; y += steps) {
+      for (let x = 0; x < context.depthWidth() - steps; x += steps) {
+        index = x + y * context.depthWidth();
+        if (depthMap[index] > 10 && depthMap[index] < 1000) {
+          if (depthMap[index + steps] > 10 &&
+            depthMap[index + steps] < 1000) {
+            if (depthMap[index + steps * context.depthWidth()] > 10 &&
+              depthMap[index + steps * context.depthWidth()] < 1000) {
+              if (depthMap[index + steps * context.depthWidth() + steps] > 10 &&
+                depthMap[index + steps * context.depthWidth() + steps] < 1000) {
+                // draw the projected point
+                //        realWorldPoint = context.depthMapRealWorld()[index];
+                p.beginShape(p.TRIANGLE_STRIP);
+                realWorldPoint = realWorldMap[index + steps * context.depthWidth()];
+                p.vertex(realWorldPoint.x, realWorldPoint.y, points[index + steps * context.depthWidth()]);  // make realworld z negative, in the 3d drawing coordsystem +z points in the direction of the eye
+                realWorldPoint = realWorldMap[index];
+                p.vertex(realWorldPoint.x, realWorldPoint.y, points[index]);  // make realworld z negative, in the 3d drawing coordsystem +z points in the direction of the eye
+                realWorldPoint = realWorldMap[index + steps * context.depthWidth() + steps];
+                p.vertex(realWorldPoint.x, realWorldPoint.y, points[index + steps * context.depthWidth() + steps]);  // make realworld z negative, in the 3d drawing coordsystem +z points in the direction of the eye
+                realWorldPoint = realWorldMap[index + steps];
+                p.vertex(realWorldPoint.x, realWorldPoint.y, points[index + steps]);  // make realworld z negative, in the 3d drawing coordsystem +z points in the direction of the eye
+                p.endShape();
+              }
+            }
+          }
         }
         //println("x: " + x + " y: " + y);
       }
     }
-    p.endShape();
 
     // draw the kinect cam
-    context.drawCamFrustum();
+    // context.drawCamFrustum();
   }
 };
 
