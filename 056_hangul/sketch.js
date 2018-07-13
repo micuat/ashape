938a01b249b7865165bd464a29952781a34f8a96
micuat <micuat@gmail.com>
2018-07-13 12:15:28 +0900

    056 random walk

--- 056_hangul/sketch.js
+++ 056_hangul/sketch.js
@@ -86,47 +86,77 @@ var s = function (p) {
     defaultShader.set("shadowMap", shadowMap);
   }
 
+  let tween = 0;
+
+  function vowel_a (angle) {
+    let cos = Math.cos(angle);
+    if(cos < 0) cos = 0;
+    let x = 50 + 50 * Math.pow(cos, 32.0);
+    let z = 100 * Math.sin(angle);
+    return {x: x, z: z, cx: -50, cz: 0};
+  }
+  function vowel_i (angle) {
+    let x = 50;
+    let z = 100 * Math.sin(angle);
+    return {x: x, z: z, cx: -50, cz: 0};
+  }
+  function vowel_o (angle) {
+    let cos = Math.cos(angle);
+    if(cos < 0) cos = 0;
+    let x = 75 * Math.sin(angle);
+    let z = 50 - 50 * Math.pow(cos, 32.0);
+    return {x: x, z: z, cx: 0, cz: -50};
+  }
+  function vowel_oo (angle) {
+    let cos = Math.cos(angle);
+    if(cos < 0) cos = 0;
+    let x = 75 * Math.sin(angle);
+    let z = 50 + 50 * Math.pow(cos, 32.0);
+    return {x: x, z: z, cx: 0, cz: -50};
+  }
+  let vowels = [vowel_a, vowel_i, vowel_o, vowel_oo];
+  let vowel0, vowel1;
+
+  function consonant_ng (angle, pos) {
+    let x = 50 * Math.cos(angle) + pos.cx;
+    let z = 50 * Math.sin(angle) + pos.cz;
+    return {x: x, z: z};
+  }
+  function consonant_m (angle, pos) {
+    let x = 50 * Math.pow(Math.abs(Math.cos(angle)), 0.5) * (Math.cos(angle)>0?1:-1) + pos.cx;
+    let z = 50 * Math.pow(Math.abs(Math.sin(angle)), 0.5) * (Math.sin(angle)>0?1:-1) + pos.cz;
+    return {x: x, z: z};
+  }
+  let consonants = [consonant_m, consonant_ng];
+  let consonant0, consonant1;
+
   function renderLandscape(canvas, shader) {
     canvas.pushMatrix();
-    let offset = -p.frameCount * 0.01;
-    let n = 4.0;
+    let n = 2.0;
 
     let x = 0;
     let z = 0;
 
-    n = 2.0;
-    function vowel_a (angle) {
-      let cos = Math.cos(angle);
-      if(cos < 0) cos = 0;
-      let x = 50 + 50 * Math.pow(cos, 32.0);
-      let z = 100 * Math.sin(angle);
-      return {x: x, z: z, cx: -50, cz: 0};
+    function lerp(p0, p1, rate) {
+      return {x: p.lerp(p0.x, p1.x, rate),
+      z: p.lerp(p0.z, p1.z, rate)};
     }
-    function vowel_o (angle) {
-      let cos = Math.cos(angle);
-      if(cos < 0) cos = 0;
-      let x = 75 * Math.sin(angle);
-      let z = 50 - 50 * Math.pow(cos, 32.0);
-      return {x: x, z: z, cx: 0, cz: -50};
-    }
-    function consonant_ng (angle, pos) {
-      let x = 50 * Math.cos(angle) + pos.cx;
-      let z = 50 * Math.sin(angle) + pos.cz;
-      return {x: x, z: z};
-    }
-
     canvas.fill(120.0, 120.0, 0, 255);
     for(let i = 0; i < n; i++) {
       let phase = Math.PI * i / n * 2.0;
-      let angle = p.millis() * 0.0025 + phase;
+      let angle = p.millis() * 0.005 + phase;
 
-      let pos = vowel_o(angle);
+      let pos0 = vowel0(angle);
+      let pos1 = vowel1(angle);
+      let pos = lerp(pos0, pos1, tween);
       canvas.pushMatrix();
       canvas.translate(pos.x, -12, pos.z);
       canvas.sphere(12);
       canvas.popMatrix();
 
-      pos = consonant_ng(angle, pos);
+      pos0 = consonant0(angle, pos0);
+      pos1 = consonant1(angle, pos1);
+      pos = lerp(pos0, pos1, tween);
       canvas.pushMatrix();
       canvas.translate(pos.x, -12, pos.z);
       canvas.sphere(12);
@@ -139,11 +169,32 @@ var s = function (p) {
   }
 
   p.draw = function () {
-    if (p.frameCount % 60 == 0) {
+    if (vowel0 == null) vowel0 = vowels[0];
+    if (consonant0 == null) consonant0 = consonants[0];
+    if (vowel1 == null) vowel1 = vowels[0];
+    if (consonant1 == null) consonant1 = consonants[0];
+    if (p.frameCount % 120 == 0) {
       print(p.frameRate());
-      defaultShader = p.loadShader(name + ("/default.frag"), name + ("/default.vert"));
+      // defaultShader = p.loadShader(name + ("/default.frag"), name + ("/default.vert"));
       p.shader(defaultShader);
     }
+    if (p.frameCount % 120 == 0) {
+      // vowel1 = vowel0;
+      vowel0 = p.random(vowels);
+      // consonant1 = consonant0;
+      consonant0 = p.random(consonants);
+    }
+    else if (p.frameCount % 120 == 60) {
+      // vowel0 = vowel1;
+      vowel1 = p.random(vowels);
+      // consonant0 = consonant1;
+      consonant1 = p.random(consonants);
+    }
+
+    tween0 = Math.cos(p.frameCount / 120.0 * Math.PI * 2.0);
+    tween0 = (tween0 == 0) ? 0.5 : (tween0 > 0 ? 1 : 0);
+    tween = p.lerp(tween, tween0, 0.1);
+
     let viewMatrix = new Packages.processing.core.PMatrix3D(
       0.5, 0.0, 0.0, 0.5,
       0.0, 0.5, 0.0, 0.5,
