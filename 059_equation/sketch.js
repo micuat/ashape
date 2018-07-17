43362c2d7e9924f4bf51aabf600bb5c1743eb90d
micuat <micuat@gmail.com>
2018-07-17 14:45:59 +0900

    059 pg and more

--- 059_equation/sketch.js
+++ 059_equation/sketch.js
@@ -3,7 +3,7 @@ var s = function (p) {
   let rfont, rfontc;
   let xs = [];
   let ys = [];
-  let rotation;
+  let pg;
   p.setup = function () {
     p.createCanvas(800, 800);
     p.frameRate(30);
@@ -12,10 +12,14 @@ var s = function (p) {
     Packages.geomerative.RCommand.setSegmentator(Packages.geomerative.RCommand.UNIFORMLENGTH);
 
     rfontc = new Packages.geomerative.RFont("assets/l_10646.ttf", 60, p.CENTER);
+
+    pg = p.createGraphics(p.width, p.height, p.P3D);
   }
 
   let curveFunction;
   let letterFunction;
+  let rotationFunction;
+  let pgBackgroundFunction;
   p.draw = function () {
     p.background(0);
 
@@ -38,7 +42,7 @@ var s = function (p) {
           // let y1 = y0;
           // let x1 = x0 + p.random(-400,400) * (1.0 - t % 1.0);
           // let y1 = y0 + p.random(-400,400) * (1.0 - t % 1.0);
-          p.line(x0, y0, x1, y1 + 1);
+          p.line(x0, y0, x1, y1 - 3);
           // p.line(x0, y0, x0 + 2, y0);
         }
         ,
@@ -52,7 +56,7 @@ var s = function (p) {
           // if(Math.pow(p.random(1.0), 4.0) > t % 1.0)
           // if (Math.abs(x0 - x1) < 1) x1 = x0;
           // if (Math.abs(y0 - y1) < 1) y1 = y0;
-          p.line(x0, y0, x1, y1 + 1);
+          p.line(x0, y0, x1, y1 - 3);
         }
       ];
       letterFunction = p.random(funcs);
@@ -60,16 +64,89 @@ var s = function (p) {
       funcs = [
         function (t) { }
         ,
-        function (t) { p.rotateZ(Math.pow(Math.min(t * 2.0, 1.0), 0.25) * Math.PI * 0.5); }
+        function (t) { pg.rotateZ(Math.pow(Math.min(t * 2.0, 1.0), 0.25) * Math.PI * 0.5); }
         ,
-        function (t) { p.rotateY(Math.pow(Math.min(t, 1.0), 0.5) * Math.PI); }
+        function (t) { pg.rotateY(t * Math.PI * 0.5); }
+        ,
+        function (t) { pg.rotateY(Math.pow(Math.min(t, 1.0), 0.5) * Math.PI); }
+        ,
+        function (t) { pg.rotateX(Math.pow(Math.min(t, 1.0), 0.5) * Math.PI * 0.5); }
       ];
-      rotation = p.random(funcs);
+      rotationFunction = p.random(funcs);
+
+      funcs = [
+        function () { pg.background(0); }
+        ,
+        function () { pg.background(0); }
+        ,
+        function () { pg.background(0); }
+        ,
+        function () {
+          pg.noStroke();
+          pg.fill(0, 10);
+          pg.rect(0, 0, pg.width, pg.height);
+        }
+      ];
+      pgBackgroundFunction = p.random(funcs);
+
+      pg.beginDraw();
+      pg.background(0);
+      pg.endDraw();
 
       for (let i = 0; i < 21; i++) {
         xs[i] = ys[i] = 0;
       }
     }
+
+    pg.beginDraw();
+    pgBackgroundFunction();
+    pg.endDraw();
+
+    if (t % 4 >= 1) {
+      let n = 300;
+      let dn = n / 10;
+      for (let i = -10; i <= 10; i++) {
+        let dl = n * curveFunction(i);
+        // dl = Math.min(dl, 180);
+        xs[i + 10] = p.lerp(xs[i + 10], dl, 0.1);
+      }
+      for (let j = -10; j <= 10; j++) {
+        let dl = n * Math.cos(j / 10 * Math.PI * 0.5);
+        ys[j + 10] = p.lerp(ys[j + 10], dl, 0.1);
+      }
+      function drawSystem(broken) {
+        for (let i = -10; i <= 10; i++) {
+          let i1 = p.floor(p.random(-10, 11));
+          if (!broken || p.random(1) < 0.9) i1 = i;
+          let dl0 = xs[i + 10];
+          let dl1 = xs[i1 + 10];
+          pg.line(-dl0, i * dn, dl1, i1 * dn);
+        }
+        for (let j = -10; j <= 10; j++) {
+          let dl = ys[j + 10];
+          pg.line(j * dn, -dl, j * dn, dl);
+        }
+      }
+
+      pg.beginDraw();
+      pg.stroke(255);
+      pg.translate(pg.width / 2, pg.height / 2);
+      rotationFunction((t - 1) % 4.0);
+      pg.pushMatrix();
+      pg.translate(-pg.width / 4, 0);
+      pg.rotateY(Math.PI * 0.25);
+      drawSystem(false);
+      pg.popMatrix();
+
+      pg.pushMatrix();
+      pg.translate(pg.width / 4, 0);
+      pg.rotateY(Math.PI * -0.25);
+      drawSystem(true);
+      pg.popMatrix();
+      pg.endDraw();
+    }
+
+    p.image(pg, 0, 0)
 
     p.push();
     p.translate(p.width / 2, p.height / 2);
@@ -94,44 +171,6 @@ var s = function (p) {
       drawText(grp0);
       p.translate(0, 100);
       drawText(grp1);
-      p.pop();
-    }
-    if (t % 4 >= 1) {
-      let n = 300;
-      let dn = n / 10;
-      for (let i = -10; i <= 10; i++) {
-        let dl = n * curveFunction(i);
-        // dl = Math.min(dl, 180);
-        xs[i + 10] = p.lerp(xs[i + 10], dl, 0.1);
-      }
-      for (let j = -10; j <= 10; j++) {
-        let dl = n * Math.cos(j / 10 * Math.PI * 0.5);
-        ys[j + 10] = p.lerp(ys[j + 10], dl, 0.1);
-      }
-      function drawSystem(broken) {
-        for (let i = -10; i <= 10; i++) {
-          let i1 = p.floor(p.random(-10, 11));
-          if (!broken || p.random(1) < 0.9) i1 = i;
-          let dl0 = xs[i + 10];
-          let dl1 = xs[i1 + 10];
-          p.line(-dl0, i * dn, dl1, i1 * dn);
-        }
-        for (let j = -10; j <= 10; j++) {
-          let dl = ys[j + 10];
-          p.line(j * dn, -dl, j * dn, dl);
-        }
-      }
-      rotation((t - 1) % 4.0);
-      p.push();
-      p.translate(-p.width / 4, 0);
-      p.rotateY(Math.PI * 0.25);
-      drawSystem(false);
-      p.pop();
-
-      p.push();
-      p.translate(p.width / 4, 0);
-      p.rotateY(Math.PI * -0.25);
-      drawSystem(true);
       p.pop();
     }
     p.pop();
