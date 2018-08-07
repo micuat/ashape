bfe403970902ed6890864538bed3145e9470d858
micuat <micuat@gmail.com>
2018-08-08 01:38:15 +0900

    075

--- 075_wresting/sketch.js
+++ 075_wresting/sketch.js
@@ -21,6 +21,10 @@ var s = function (p) {
   let pgBackgroundFunction;
   let doDoubleDraw;
   let displaceN;
+  let curRotX = 0;
+  let destRotX = 0;
+  let curRotY = 0;
+  let destRotY = 0;
   p.draw = function () {
     p.background(0);
 
@@ -29,12 +33,12 @@ var s = function (p) {
 
     if (curveFunctionX == null || p.frameCount % (30 * 4) == 0) {
       let funcs = [
-        function (i) { return Math.cos(i / 10 * Math.PI * 0.5) }
+        function (i) { return Math.sqrt(Math.cos(i / 10 * Math.PI * 0.5)) }
         ,
-        function (i) { return Math.cos(i / 10 * Math.PI * 2.0) }
+        function (i) { return i * 0.1 }
         ,
-        function (i) { return Math.sin(i / 10 * Math.PI * 0.25) }
-        ,
+        // function (i) { return i * 0.05 + 0.5 }
+        // ,
         function (i) { return 1.0 }
       ];
       curveFunctionX = p.random(funcs);
@@ -76,22 +80,25 @@ var s = function (p) {
 
       funcs = [
         function () { pg.background(0); }
-        ,
-        function () { pg.background(0); }
-        ,
-        function () { pg.background(0); }
-        ,
-        function () {
-          pg.noStroke();
-          pg.fill(0, 10);
-          pg.rect(0, 0, pg.width, pg.height);
-        }
+        // ,
+        // function () { pg.background(0); }
+        // ,
+        // function () { pg.background(0); }
+        // ,
+        // function () {
+        //   pg.noStroke();
+        //   pg.fill(0, 10);
+        //   pg.rect(0, 0, pg.width, pg.height);
+        // }
       ];
       pgBackgroundFunction = p.random(funcs);
 
       displaceN = Math.floor(p.random(-3, 3));
 
-      doDoubleDraw = false;//p.random(1.0) > 0.7 ? true : false;
+      destRotX = Math.floor(p.random(-3, 3)) * Math.PI * 0.25;
+      destRotY = Math.floor(p.random(-3, 3)) * Math.PI * 0.25;
+
+      doDoubleDraw = p.random(1.0) > 0.7 ? true : false;
 
       pg.beginDraw();
       pg.background(0);
@@ -110,7 +117,9 @@ var s = function (p) {
     {
       let n = 280;
       let dn = n / 10;
-      if (t % 4 >= 1) {
+      if (t % 4 < 1) {
+      }
+      else if (t % 4 < 3) {
         for (let i = -10; i <= 10; i++) {
           let dl = n * curveFunctionX(i);
           xs[i + 10] = p.lerp(xs[i + 10], dl, 0.1);
@@ -120,31 +129,41 @@ var s = function (p) {
           ys[j + 10] = p.lerp(ys[j + 10], dl, 0.1);
         }
       }
+      else {
+        for (let i = -10; i <= 10; i++) {
+          let dl = 0;
+          xs[i + 10] = p.lerp(xs[i + 10], dl, 0.1);
+        }
+        for (let j = -10; j <= 10; j++) {
+          let dl = 0;
+          ys[j + 10] = p.lerp(ys[j + 10], dl, 0.1);
+        }
+      }
       function drawSystem(broken) {
         for (let i = -10; i <= 10; i++) {
           let dlx0, dly1;
           let i0 = i, i1 = i;
-          if (!broken || p.random(1) > ((t - 1.0) % 4.0) * 0.02) {
+          if (!broken){//} || p.noise(i * 0.8, t * 5.0) > 0.5) {
             dlx0 = xs[i0 + 10];
             dlx1 = xs[i1 + 10];
             dly0 = ys[i0 + 10];
             dly1 = ys[i1 + 10];
-            }
-          else {
-            i1 = Math.floor(p.map(p.noise(i * 0.1, t * 2.0), 0.0, 1.0, -10.0, 11.0));
-            dlx0 = xs[i0 + 10];
-            dlx1 = xs[i1 + 10];
-            dly0 = ys[i0 + 10];
-            dly1 = ys[i1 + 10];
+            pg.line(-dlx0, i0 * dn, dlx1, i1 * dn);
+            pg.line(i0 * dn, -dly0, i1 * dn, dly1);
           }
-          pg.line(-dlx0, i0 * dn, dlx1, i1 * dn);
-          pg.line(i0 * dn, -dly0, i1 * dn, dly1);
+          else {
+            // i1 = Math.floor(p.map(p.noise(i * 0.1, t * 2.0), 0.0, 1.0, -10.0, 11.0));
+            dlx0 = xs[i0 + 10] * p.noise(i * 0.8, t * 5.0);
+            dlx1 = xs[i1 + 10] * p.noise(i * 0.8, t * 5.0);
+            dly0 = ys[i0 + 10];
+            dly1 = ys[i1 + 10];
+            pg.line(-dlx0, i0 * dn, dlx1, i1 * dn);
+            pg.line(i0 * dn * p.noise(i * 0.8, t * 5.0), -dly0, i1 * dn * p.noise(i * 0.8, t * 5.0), dly1);
+          }
         }
       }
 
       pg.beginDraw();
-
-      // pg.translate(p.mouseX * 2 - 800, p.mouseY * 2 - 800, -800);
 
       if (t % 4 < 1.0) {
         let z = Math.cos((t % 1.0) * Math.PI) * displaceN;
@@ -153,8 +172,14 @@ var s = function (p) {
         pg.translate(0, 0, z);
       }
 
-      pg.stroke(100);
+      pg.stroke(155);
       pg.translate(pg.width / 2, pg.height / 2);
+      curRotX = p.lerp(curRotX, destRotX, 0.1);
+      if(Math.abs(curRotX - destRotX) < 0.01) curRotX = destRotX;
+      curRotY = p.lerp(curRotY, destRotY, 0.1);
+      if(Math.abs(curRotY - destRotY) < 0.01) curRotY = destRotY;
+      pg.rotateX(curRotX);
+      pg.rotateY(curRotY);
       rotationFunction((t - 1) % 4.0);
       if (t % 4 < 20) {
         // pg.rotateY(t)
