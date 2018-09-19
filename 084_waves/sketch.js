b5d0236d562371f418cf532780385d580a36d970
micuat <micuat@gmail.com>
2018-09-19 11:59:50 +0900

    more tween!

--- 084_waves/sketch.js
+++ 084_waves/sketch.js
@@ -14,20 +14,37 @@ var s = function (p) {
 
   function getCount() { return p.frameCount - startFrame };
 
+  function drawLine(i, n, rotTween, expTween, strokeTween) {
+    let r = 0.25 * p.width;
+    let angle = i / n + (rotTween + expTween) / n;
+    let x = Math.cos(angle * 2 * Math.PI) * r;
+    let y = Math.sin(angle * 2 * Math.PI) * r;
+    let xy = [[0, -p.height / 2], [0, p.height / 2], [p.width / 2, 0], [-p.width / 2, 0]];
+    for(let j in xy) {
+      let x0 = xy[j][0];
+      let y0 = xy[j][1];
+      p.stroke(255, strokeTween * p.constrain(p.map(p.dist(x, y, x0, y0), p.width / 2, p.width * 0.75, 1, 0), 0, 1) * 255);
+      p.line(x, y, x0, y0);
+    }
+  }
+
   p.draw = function () {
     t = (getCount() / 30.0);
 
-    let seq = Math.floor(t) % 4;
+    let cycle = 4.0;
+    let seq = Math.floor(t / 2.0) % cycle; // every 2 cycle
+    let phase = Math.floor(t) % 2; // every cycle
 
     p.background(0);
     p.fill(255);
     p.stroke(255);
 
     let n = Math.pow(2.0, seq + 2);
+    let nNext = Math.pow(2.0, seq + 2 + 1);
 
     p.translate(p.width / 2, p.height / 2);
 
-    let tween = (t / 1.0) % 1.0;
+    let tween = (t) % 1.0;
     if(tween < 0.5) {
       tween = Math.pow(tween * 2.0, 4.0) * 0.5;
     }
@@ -35,18 +52,30 @@ var s = function (p) {
       tween = 1.0 - Math.pow(2.0 - tween * 2.0, 4.0) * 0.5;
     }
 
-    for (let i = 0; i < n; i++) {
-      let r = 0.25 * p.width;
-      let angle = i / n + tween / n;
-      let x = Math.cos(angle * 2 * Math.PI) * r;
-      let y = Math.sin(angle * 2 * Math.PI) * r;
-      let xy = [[0, -p.height / 2], [0, p.height / 2], [p.width / 2, 0], [-p.width / 2, 0]];
-      p.ellipse(x, y, 4, 4);
-      for(let j in xy) {
-        let x0 = xy[j][0];
-        let y0 = xy[j][1];
-        p.stroke(255, p.constrain(p.map(p.dist(x, y, x0, y0), p.width / 2, p.width * 0.75, 1, 0), 0, 1) * 255);
-        p.line(x, y, x0, y0);
+    let rotTween, expTween;
+    if(phase == 0)
+    {
+      rotTween = tween;
+      expTween = 0;
+    }
+    else {
+      rotTween = 0;
+      expTween = tween;
+    }
+
+    for (let i = 0; i < nNext; i++) {
+      let strokeTween = 1.0;
+      let toStart = false;
+      if(seq == cycle - 1 && phase == 1) {
+        toStart = true;
+        if(i % (nNext / 8) != 0)
+          strokeTween = 1.0 - expTween;
+      }
+      drawLine(i, n, rotTween, 0, strokeTween);
+      strokeTween *= expTween;
+      if(i % 2 == 0 && toStart == false) {
+        drawLine(i, n, rotTween, expTween * 0.5, strokeTween);
+        drawLine(i, n, rotTween, expTween * -0.5, strokeTween);
       }
     }
   }
