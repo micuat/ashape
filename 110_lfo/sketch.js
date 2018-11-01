3abab9902e327dd327b6d419f5fb427ffc943680
micuat <micuat@gmail.com>
2018-11-02 01:20:17 +0900

    110 wip

--- 110_lfo/sketch.js
+++ 110_lfo/sketch.js
@@ -39,7 +39,7 @@ function Segment() {
   this.b = new p5.Vector();
   this.sw = 0;
   this.target = p.createVector();
-  this.width = 20;
+  this.width = 40;
 
   if (arguments.length == 6) {
     let x = arguments[2];
@@ -118,9 +118,11 @@ Segment.prototype.show = function (z) {
   cross.mult(w);
 
   let pLeft = this.a.copy();
-  pLeft.sub(cross);
+  if(this.isLeft)
+    pLeft.sub(cross);
   let pRight = this.a.copy();
-  pRight.add(cross);
+  if(!this.isLeft)
+    pRight.add(cross);
 
   this.pg.vertex(pLeft.x, pLeft.y, pLeft.z + z);
   this.pg.vertex(pRight.x, pRight.y, pRight.z + z);
@@ -131,14 +133,16 @@ function Tentacle(p, pg, x, y, ex, ey, index) {
   this.pg = pg;
   this.segments = new Array(30);
   this.base;
-  this.len = 40;
+  this.len = 20;
   this.defaultTarget = new p5.Vector(ex, ey);
   this.index = index;
 
   this.base = new p5.Vector(x, y);
   this.segments[0] = new Segment(p, this.pg, 300, 200, this.len, 0);
+  this.segments[0].isLeft = index > 0;
   for (let i = 1; i < this.segments.length; i++) {
     this.segments[i] = new Segment(p, this.pg, this.segments[i - 1], this.len, i);
+    this.segments[i].isLeft = index > 0;
   }
 
   this.target = p.createVector();
@@ -147,7 +151,7 @@ function Tentacle(p, pg, x, y, ex, ey, index) {
 
   this.startTime = 0;
   this.dead = true;
-  this.lifeTime = 3;
+  this.lifeTime = 3000000000;
 }
 
 Tentacle.prototype.init = function (v) {
@@ -196,29 +200,27 @@ Tentacle.prototype.show = function () {
   let d = this.p.map(this.p.millis() * 0.001 - this.startTime, 0, this.lifeTime * 0.1, 0, 1);
   d = this.p.constrain(d, 0, 1);
 
-  this.pg.stroke(255);
+  // this.pg.stroke(255);
   let total = this.segments.length;
   let end = this.segments[total - 1];
   this.pg.beginShape(this.p.TRIANGLE_STRIP);
   for (let i = 0; i < this.segments.length; i++) {
-    if (this.segments.length - i < c * this.segments.length
-      && i < d * this.segments.length)
-      this.segments[i].show((1 - c) * 100 * i);
+    this.segments[i].show(0);
   }
   this.pg.endShape();
 }
 
-var S109 = function (p) {
+var S110 = function (p) {
   let tentacles = [];
   let pg;
 
   pg = p.createGraphics(800, 800, p.P3D);
 
-  for (let i = 0; i < 32; i++) {
-    let x = p.map(i, 0, 31, -400, 400);
+  for (let i = 0; i < 2; i++) {
+    let x = p.map(i, 0, 2, -400, 400);
     let y = 0;
     let t = new Tentacle(p, pg, x, y, x, p.height, i);
-    // t.init(p.createVector(x, y));
+    t.init(p.createVector(x, y));
     tentacles.push(t);
   }
   
@@ -227,20 +229,16 @@ var S109 = function (p) {
   this.back = 0;
 
   this.spawn = function (v) {
-    for (let i = 0; i < tentacles.length; i++) {
-      let t = tentacles[i];
-      if (t.dead) {
-        t.init(v);
-        return;
-      }
-    }
+    // for (let i = 0; i < tentacles.length; i++) {
+    //   let t = tentacles[i];
+    //   if (t.dead) {
+    //     t.init(v);
+    //     return;
+    //   }
+    // }
   }
 
   this.drawPg = function(pg, t) {
-    // if(p.frameCount % 30 == 0){// && p.key == '0') {
-    //   this.spawn(p.createVector(0, 0));
-    //   this.spawn(p.createVector(0, 0));
-    // }
     pg.beginDraw();
     pg.background(0);
     // pg.background(this.back);
@@ -249,13 +247,13 @@ var S109 = function (p) {
 
     pg.pushMatrix();
 
-    // pg.noStroke();
-    // pg.fill(255);
-    pg.noFill();
+    pg.noStroke();
+    pg.fill(255);
+    // pg.noFill();
     pg.pointLight(255, 255, 255, 0, 0, 100);
     for(let i = 0; i < tentacles.length; i++) {
-      let x = p.cos(p.millis() * 0.003) * 400;
-      let y = p.sin(p.millis() * 0.001) * 400;
+      let x = p.cos(t) * (300 - EasingFunctions.easeInQuint(1 - Math.abs(Math.sin(t * Math.PI * 0.2))) * (1-i)*100);
+      let y = p.sin(t) * 300;
       let v = p.createVector(x, y);
       tentacles[i].update(v);
       tentacles[i].show();
@@ -275,7 +273,7 @@ var S109 = function (p) {
 var s = function (p) {
   let startTime;
   
-  let s109 = new S109(p);
+  let s110 = new S110(p);
 
   p.setup = function () {
     name = p.folderName;
@@ -291,16 +289,16 @@ var s = function (p) {
   p.draw = function () {
     t = getTime();
 
-    s109.draw(t);
+    s110.draw(t);
   }
 
   p.oscEvent = function (m) {
     let path = m.addrPattern().split("/");
     if (path.length >= 3 && path[1] == "sc3p5" && path[2] == "spawn") {
-      s109.spawn(p.createVector(m.get(0).floatValue(), 0));
+      s110.spawn(p.createVector(m.get(0).floatValue(), 0));
     }
     else if (path.length >= 4 && path[1] == "sc3p5" && path[2] == "control") {
-      s109[path[3]] = m.get(0).floatValue();
+      s110[path[3]] = m.get(0).floatValue();
     }
   }
 
