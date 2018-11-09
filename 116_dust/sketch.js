015aa186e2a14fa3322f9580dd805cd04eb39bb4
micuat <micuat@gmail.com>
2018-11-09 23:14:02 +0100

    116

---
+++ 116_dust/sketch.js
@@ -0,0 +1,197 @@
+// https://gist.github.com/gre/1650294
+EasingFunctions = {
+  // no easing, no acceleration
+  linear: function (t) { return t },
+  // accelerating from zero velocity
+  easeInQuad: function (t) { return t * t },
+  // decelerating to zero velocity
+  easeOutQuad: function (t) { return t * (2 - t) },
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: function (t) { return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t },
+  // accelerating from zero velocity 
+  easeInCubic: function (t) { return t * t * t },
+  // decelerating to zero velocity 
+  easeOutCubic: function (t) { return (--t) * t * t + 1 },
+  // acceleration until halfway, then deceleration 
+  easeInOutCubic: function (t) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 },
+  // accelerating from zero velocity 
+  easeInQuart: function (t) { return t * t * t * t },
+  // decelerating to zero velocity 
+  easeOutQuart: function (t) { return 1 - (--t) * t * t * t },
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: function (t) { return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t },
+  // accelerating from zero velocity
+  easeInQuint: function (t) { return t * t * t * t * t },
+  // decelerating to zero velocity
+  easeOutQuint: function (t) { return 1 + (--t) * t * t * t * t },
+  // acceleration until halfway, then deceleration 
+  easeInOutQuint: function (t) { return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t }
+}
+
+var p;
+
+var S116 = function (p) {
+  let pg, pgTex;
+
+  pg = p.createGraphics(800, 800, p.P3D);
+  pgTex = p.createGraphics(800, 800, p.P3D);
+
+  this.pattern = 0;
+  this.back = 0;
+  this.targetRot = 0;
+  this.curRot = 0;
+  this.onOsc = function (path) {
+    if(path == "sig2") {
+      for(let i = 0; i < points.length; i++) {
+        points[i].v = p5.Vector.random2D();
+      }
+      // this.back = 1;
+      // this.targetRot = Math.PI * Math.floor(p.random(-2, 3));
+    }
+    else if(path == "sig") {
+      this.pattern = (this.pattern + 1) % 3;
+    }
+  }
+
+  let points = [];
+  for(let i = 0; i < 20; i++) {
+    let po = p.createVector(p.random(-400, 400), p.random(-400, 400));
+    let v = p5.Vector.random2D();
+    points.push({p: po, v: v});
+  }
+  this.drawPg = function(pg, t) {
+    pgTex.beginDraw();
+    pgTex.pushMatrix();
+    // pgTex.background(255 * this.back);
+    pgTex.translate(pgTex.width * 0.5, pgTex.height * 0.5);
+    pgTex.popMatrix();
+    pgTex.endDraw();
+
+    pg.beginDraw();
+    pg.textureMode(p.NORMAL);
+    pg.background(255 * this.back);
+    this.back = p.lerp(this.back, 0, 0.02);
+
+    pg.translate(pg.width * 0.5, pg.height * 0.5);
+    pg.pushMatrix();
+    // pg.rotate(this.curRot);
+    this.curRot = p.lerp(this.curRot, this.targetRot, 0.1);
+
+    pg.stroke(255);
+    pg.strokeWeight(1);
+    this.drawLine = function (x0, y0, x1, y1) {
+      pg.pushMatrix();
+      let xm = p.lerp(x0, x1, 0.5);
+      let ym = p.lerp(y0, y1, 0.5);
+      pg.translate(xm, ym);
+      let angle = Math.atan2(y1 - y0, x1 - x0);
+      pg.rotate(angle);
+      let s = Math.sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
+      pg.scale(s, s);
+      pg.strokeWeight(5.0 / s);
+
+      if (this.pattern == 0) {
+        pg.line(-0.5, 0, -0.125, 0);
+        pg.line(0.5, 0, 0.125, 0);
+        let l = 0.25 / Math.sqrt(2);
+        pg.rotate(Math.PI / 4);
+        pg.rect(-l/2, -l/2, l, l);
+      }
+      else if (this.pattern == 1) {
+        pg.noFill();
+        pg.curveTightness(0.0);
+        for(let i = 0; i < 3; i++) {
+          let h = 0.05 * (i + 1);
+          pg.beginShape();
+          pg.curveVertex(-0.5, 0);
+          pg.curveVertex(-0.5, 0);
+          pg.curveVertex(-0.25, h * 0.1);
+          pg.curveVertex(0, h);
+          pg.curveVertex(0.25, h * 0.1);
+          pg.curveVertex(0.5, 0);
+          pg.curveVertex(0.5, 0);
+          pg.endShape();
+  
+          pg.beginShape();
+          pg.curveVertex(-0.5, 0);
+          pg.curveVertex(-0.5, 0);
+          pg.curveVertex(-0.25, -h * 0.1);
+          pg.curveVertex(0, -h);
+          pg.curveVertex(0.25, -h * 0.1);
+          pg.curveVertex(0.5, 0);
+          pg.curveVertex(0.5, 0);
+          pg.endShape();
+          }
+      }
+      else {
+        pg.line(-0.5, 0, -0.125, 0);
+        pg.line(0.5, 0, 0.125, 0);
+        let l = 0.25 / Math.sqrt(2);
+        pg.rotate(Math.PI / 4);
+        pg.ellipse(0, 0, 0.25, 0.25);
+      }
+      pg.popMatrix();
+    }
+    for(let i = 0; i < points.length; i++) {
+      pg.point(points[i].p.x, points[i].p.y);
+      for(let j = i; j < points.length; j++) {
+        let alpha = 1 - points[i].p.dist(points[j].p) / 300.0;
+        if(alpha > 0) {
+          alpha = Math.min(alpha * 2.0, 1.0);
+          pg.stroke(255, alpha * 255);
+          this.drawLine(points[i].p.x, points[i].p.y, points[j].p.x, points[j].p.y);
+        }
+      }
+    }
+    for(let i = 0; i < points.length; i++) {
+      points[i].p.add(points[i].v);
+      points[i].p.x = (points[i].p.x + 1000 * 1.5) % 1000 - 1000 * 0.5;
+      points[i].p.y = (points[i].p.y + 1000 * 1.5) % 1000 - 1000 * 0.5;
+      // points[i].p.x = (points[i].p.x + pg.width * 1.5) % pg.width - pg.width * 0.5;
+      // points[i].p.y = (points[i].p.y + pg.width * 1.5) % pg.width - pg.width * 0.5;
+    }
+
+    pg.popMatrix();
+    pg.endDraw();
+  }
+
+  this.draw = function (t) {
+    this.drawPg(pg, t);
+    p.image(pg, 0, 0);
+  }
+};
+
+var s = function (p) {
+  let startTime;
+  
+  let s116 = new S116(p);
+
+  p.setup = function () {
+    name = p.folderName;
+
+    p.createCanvas(800, 800);
+    p.frameRate(60);
+
+    startTime = p.millis();
+  }
+
+  function getTime() { return (p.millis() - startTime) * 0.001 };
+
+  p.draw = function () {
+    t = getTime();
+
+    s116.draw(t);
+  }
+
+  p.oscEvent = function (m) {
+    let path = m.addrPattern().split("/");
+    if (path.length >= 3 && path[1] == "sc3p5") {
+      s116.onOsc(path[2]);
+    }
+  }
+
+  p.mousePressed = function () {
+  }
+};
+
+p = new p5(s);
