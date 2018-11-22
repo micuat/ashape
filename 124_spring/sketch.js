28e71a6a4b27bfcc38e571012facb0566759b683
micuat <micuat@gmail.com>
2018-11-22 16:44:55 +0100

    124 wip

--- 124_spring/sketch.js
+++ 124_spring/sketch.js
@@ -13,6 +13,8 @@ var s = function (p) {
   let zoff = 0;
   
   let physics;
+
+  let colors = [{r: 0x61,g: 0x21,b: 0x0f},{r: 0xea,g: 0x2b,b: 0x1f},{r: 0xff,g: 0xee,b: 0xdb},{r: 0xed,g: 0xae,b: 0x49},{r: 0xf9,g: 0xdf,b: 0x74}];
   
   p.setup = function () {
     p.createCanvas(800, 800);
@@ -80,59 +82,83 @@ var s = function (p) {
     p.translate(p.width / 2, p.height / 2);
     physics.update();
 
-    pgTex.beginDraw();
-    pgTex.stroke(255);
-    // pgTex.noStroke();
-    let xoff = 0;
     let w = pgTex.width / cols;
     let h = pgTex.height / rows;
+    pcolors = [];
     for (let i = 0; i < cols; i++) {
-      let yoff = 0;
+      pcolors[i] = [];
       for (let j = 0; j < rows; j++) {
-        //particles[i][j].display();
-        // let windy = p.map(p.noise(xoff, yoff, zoff), 0, 1, 0, 3);
-        // let windx = p.map(p.noise(xoff + 5000, yoff + 5000, zoff), 0, 1, -0.5, 0);
-        // let windz = p.map(p.noise(xoff + 3000, yoff + 3000, zoff), 0, 1, -1, 1);
-        // let wind = new Packages.toxi.geom.Vec3D(windx*2, windy*2, windz*2);
-        let t = p.millis() * 0.001;
         let windx = (p.mouseX - p.width / 2) / p.width * 2.0;
         let windy = (p.mouseY - p.height / 2) / p.height * 2.0;
         let windz = 0;
         let wind = new Packages.toxi.geom.Vec3D(windx, windy, windz);
         particles[i][j].get().addForce(wind);
-        let vx = particles[i][j].get().getVelocity().x * 100;// + 50;
-        let vy = particles[i][j].get().getVelocity().y * 100;// + 50;
-        let vz = particles[i][j].get().getVelocity().z * 100;// + 50;
-        pgTex.fill(vx, vy, vz);
-        // pgTex.fill(windx*-500, windy*100, windz*100+100);
-        pgTex.rect(w*i, h*j, w, h)
-        yoff += 0.02;
+        let vel = particles[i][j].get().getVelocity();
+        // let vx = vel.x * 100;
+        // let vy = vel.y * 100;
+        // let vz = vel.z * 100;
+        let vamp = p.constrain(vel.magnitude(), 0.5, 1);
+        let vrx = p.lerp(colors[1].r, colors[3].r, 0.5 + 0.5 * p.constrain(vel.x, -1, 1));
+        let vgx = p.lerp(colors[1].g, colors[3].g, 0.5 + 0.5 * p.constrain(vel.x, -1, 1));
+        let vbx = p.lerp(colors[1].b, colors[3].b, 0.5 + 0.5 * p.constrain(vel.x, -1, 1));
+        let vry = p.lerp(colors[2].r, colors[4].r, 0.5 + 0.5 * p.constrain(vel.y, -1, 1));
+        let vgy = p.lerp(colors[2].g, colors[4].g, 0.5 + 0.5 * p.constrain(vel.y, -1, 1));
+        let vby = p.lerp(colors[2].b, colors[4].b, 0.5 + 0.5 * p.constrain(vel.y, -1, 1));
+        let vxy = Math.abs(vel.y) / Math.sqrt(vel.x * vel.x + vel.y * vel.y + 0.01);
+        let vr = p.lerp(vrx, vry, vxy) * vamp;
+        let vg = p.lerp(vgx, vgy, vxy) * vamp;
+        let vb = p.lerp(vbx, vby, vxy) * vamp;
+        pcolors[i][j] = {r: vr, g: vg, b: vb};
+        // pcolors[i][j] = {r: vamp, g: vamp, b: vamp};
       }
-      xoff += 0.02;
     }
-    zoff += 0.02;
-    pgTex.endDraw();
 
-    // p.pointLight(255, 255, 255, 0, 0, 300);
-    p.noFill();
+    p.background(colors[2].r, colors[2].g, colors[2].b);
+
+    p.pointLight(255, 255, 255, 0, 0, 300);
     p.noStroke();
-    p.textureMode(p.NORMAL);
+    p.ambientLight(colors[1].r, colors[1].g, colors[1].b);
+    // p.textureMode(p.NORMAL);
+    p.fill(colors[3].r, colors[3].g, colors[3].b);
     p.rotateX(Math.PI / 3.0);
     for (let j = 0; j < rows - 1; j++) {
       p.beginShape(p.TRIANGLE_STRIP);
-      p.texture(pgTex);
+      // p.texture(pgTex);
       for (let i = 0; i < cols; i++) {
         let x1 = particles[i][j].get().x;
         let y1 = particles[i][j].get().y;
         let z1 = particles[i][j].get().z;
-        let u = p.map(i, 0, cols - 1, 0, 1);
-        let v1 = p.map(j, 0, rows - 1, 0, 1);
-        p.vertex(x1, y1, z1, u, v1);
         let x2 = particles[i][j + 1].get().x;
         let y2 = particles[i][j + 1].get().y;
         let z2 = particles[i][j + 1].get().z;
-        let v2 = p.map(j + 1, 0, rows - 1, 0, 1);
-        p.vertex(x2, y2, z2, u, v2);
+        let ii = i + 1;
+        if(ii >= cols) ii = cols - 1;
+        let x3 = particles[ii][j].get().x;
+        let y3 = particles[ii][j].get().y;
+        let z3 = particles[ii][j].get().z;
+        // p.fill(pcolors[i][j].r, pcolors[i][j].g, pcolors[i][j].b);
+        p.normal(z3 - z1, z2 - z1, 1);
+        p.vertex(x1, y1, z1);
+        // p.fill(pcolors[i][j + 1].r, pcolors[i][j + 1].g, pcolors[i][j + 1].b);
+        p.normal(z3 - z1, z2 - z1, 1);
+        p.vertex(x2, y2, z2);
+      }
+      p.endShape();
+    }
+    p.stroke(colors[0].r, colors[0].g, colors[0].b, 128);
+    for (let j = 0; j < rows - 1; j++) {
+      for (let i = 0; i < cols - 1; i++) {
+        let x1 = particles[i][j].get().x;
+        let y1 = particles[i][j].get().y;
+        let z1 = particles[i][j].get().z;
+        let x2 = particles[i][j + 1].get().x;
+        let y2 = particles[i][j + 1].get().y;
+        let z2 = particles[i][j + 1].get().z;
+        p.line(x1, y1, z1, x2, y2, z2);
+        x2 = particles[i + 1][j].get().x;
+        y2 = particles[i + 1][j].get().y;
+        z2 = particles[i + 1][j].get().z;
+        p.line(x1, y1, z1, x2, y2, z2);
       }
       p.endShape();
     }
@@ -143,6 +169,9 @@ var s = function (p) {
 
   }
 
+  p.oscEvent = function(m) {
+    print(m);
+  }
 };
 
 var p = new p5(s);
